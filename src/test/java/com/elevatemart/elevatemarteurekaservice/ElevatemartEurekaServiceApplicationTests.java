dc28a7607822388bdea63247b1a68dff46024948
shoaib-ahmed16 <shoaib-ahmed16>
2024-03-17 22:19:01 +0530

    eureka server

---
+++ src/test/java/com/elevatemart/elevatemarteurekaservice/ElevatemartEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevatemart.elevatemarteurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatemartEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
